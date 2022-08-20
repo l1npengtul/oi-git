7e19501ecacce592c86468900a2b3b8cc1b76a3c
Jonah <jnhev42@gmail.com>
2022-08-20 15:55:03 +0100

    changing state for asset loader
    no longer sets initial state

--- src/state.rs
+++ src/state.rs
@@ -2,16 +2,8 @@ use crate::prelude::*;
 
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
 pub enum GameState {
+    AssetLoading,
     MainMenu,
     InGame,
     Paused,
-}
-
-pub struct StatePlugin {
-    pub initial: GameState,
-}
-impl Plugin for StatePlugin {
-    fn build(&self, app: &mut App) {
-        app.add_state(self.initial.clone());
-    }
 }
