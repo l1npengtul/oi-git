590cd16f87132dec74a30aaf9a48ff1384668a43
Jonah <jnhev42@gmail.com>
2022-08-22 00:15:00 +0100

    terminal resize

--- src/terminal.rs
+++ src/terminal.rs
@@ -4,7 +4,7 @@ pub mod conv_cp437;
 mod text_sprite;
 pub use text_sprite::*;
 
-pub const TERM_DIM: (f32, f32) = (640.0, 480.0);
+pub const TERM_DIM: (f32, f32) = (1280.0, 960.0);
 pub const TERM_W: f32 = TERM_DIM.0;
 pub const TERM_H: f32 = TERM_DIM.1;
 
