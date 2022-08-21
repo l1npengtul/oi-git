43b310d65c3d1d37656a1a3b87e225e09a8c1a32
Jonah <jnhev42@gmail.com>
2022-08-21 15:53:00 +0100

    text sprite refactor

--- src/terminal/text_sprite.rs
+++ src/terminal/text_sprite.rs
@@ -23,73 +23,136 @@ pub const ATLAS_CHAR_H: f32 = 64.;
 pub struct TextSpritePlugin;
 
 impl Plugin for TextSpritePlugin {
-    fn build(&self, app: &mut App) {
-        app.add_enter_system(
-            GameState::MainMenu, spawn_with_builder,
-        );
-    }
+    fn build(&self, _: &mut App) {}
 }
 
-fn spawn_with_builder(mut commands: Commands, font: Res<FontAtlas>) {
-    info!("spawning");
-    let all_chars = " a burger\nat night";
-
-    let builder = TextSpriteBuilder {
-        text: all_chars.to_string(),
-        atlas: font.atlas.clone(),
-        scale: 0.3,
-    };
-    builder.spawn(&mut commands, |_| {}, |_| {});
-}
-
-#[derive(Component)]
-struct TextSprite;
-
-pub struct TextSpriteBuilder {
+#[derive(Component, Debug, Clone)]
+pub struct TextSprite {
     pub text: String,
     pub atlas: Handle<TextureAtlas>,
     pub scale: f32,
+    pub chars: Vec<Entity>,
 }
 
-impl TextSpriteBuilder {
+#[derive(Bundle)]
+pub struct TextSpriteBundle {
+    pub this: TextSprite,
+    #[bundle]
+    pub vis: VisibilityBundle,
+    #[bundle]
+    pub trans: TransformBundle,
+}
+
+#[derive(Component, Debug, Clone)]
+pub struct TextSpriteChar;
+
+#[derive(Bundle)]
+pub struct TextSpriteCharBundle {
+    this: TextSpriteChar,
+    #[bundle]
+    sprite: SpriteSheetBundle,
+}
+
+pub fn iter_row_col(s: &str) -> impl Iterator<Item = (usize, usize, char)> + '_ {
+    s.lines()
+        .enumerate()
+        .flat_map(|(ri, col)| col.chars().enumerate().map(move |(ci, ch)| (ri, ci, ch)))
+}
+
+impl TextSprite {
+    pub fn new(text: String, atlas: Handle<TextureAtlas>, scale: f32) -> Self {
+        Self {
+            text,
+            atlas,
+            scale,
+            chars: Vec::new(),
+        }
+    }
     /// Spawns the text stored in the TextSpriteBuilder
     /// Takes closures to be applied to every entity spawned
     /// If further modifications are required
     pub fn spawn<ChildModifier, ParentModifier>(
-        &self,
+        mut self,
         commands: &mut Commands,
-        mut child: ChildModifier,
-        parent: ParentModifier,
+        child_modifier: ChildModifier,
+        parent_modifier: ParentModifier,
     ) where
         ChildModifier: FnMut(EntityCommands),
         ParentModifier: FnOnce(EntityCommands),
     {
-        let mut parent_entity = commands.spawn_bundle(VisibilityBundle::default());
-        parent_entity.insert_bundle(TransformBundle::default());
-        parent_entity.insert(TextSprite);
-        let (w, h) = (self.scale * ATLAS_CHAR_W, self.scale * ATLAS_CHAR_H);
+        let mut parent = commands.spawn();
+        parent.add_children(|builder| self.spawn_chars(builder, child_modifier, 0));
+        parent.insert_bundle(TextSpriteBundle {
+            this: self.clone(),
+            vis: VisibilityBundle::default(),
+            trans: TransformBundle::default(),
+        });
+        parent_modifier(parent)
+    }
 
-        parent_entity.add_children(|builder| {
-            for (ln, i, ch) in self
-                .text
-                .lines()
-                .enumerate()
-                .flat_map(|(ln, s)| s.chars().enumerate().map(move |(i, ch)| (ln, i, ch)))
-            {
-                let child_entity = builder.spawn_bundle(SpriteSheetBundle {
-                    sprite: TextureAtlasSprite::new(conv_cp437::index_of(ch)),
+    /// `offset` is the number of chars to skip before spawning new ones
+    pub fn spawn_chars<ChildModifier>(
+        &mut self,
+        builder: &mut ChildBuilder,
+        mut child_modifier: ChildModifier,
+        offset: usize,
+    ) where
+        ChildModifier: FnMut(EntityCommands),
+    {
+        let (w, h) = (self.scale * ATLAS_CHAR_W, self.scale * ATLAS_CHAR_H);
+        assert!(
+            self.len() >= offset,
+            "cannot offset by more than the text's length"
+        );
+        for (ri, ci, ch) in iter_row_col(&self.text).skip(offset) {
+            let child_commands = builder.spawn_bundle(TextSpriteCharBundle {
+                this: TextSpriteChar,
+                sprite: SpriteSheetBundle {
+                    sprite: TextureAtlasSprite::new(conv_cp437::index_of(ch).unwrap()),
                     texture_atlas: self.atlas.clone(),
                     transform: Transform {
                         scale: Vec3::splat(self.scale),
-                        translation: Vec3::new(i as f32 * w, ln as f32 * -h, 0.0),
+                        translation: Vec3::new(ci as f32 * w, ri as f32 * -h, 0.0),
                         ..default()
                     },
-                    ..Default::default()
-                });
-                child(child_entity);
-            }
-        });
+                    ..default()
+                },
+            });
+            self.chars.push(child_commands.id());
+            child_modifier(child_commands);
+        }
+    }
+    /// length disregarding linebreaks
+    pub fn len(&self) -> usize {
+        self.text.lines().map(|ln| ln.len()).sum()
+    }
 
-        parent(parent_entity)
+    pub fn add_str<ChildModifier>(
+        &mut self,
+        s: &str,
+        commands: &mut Commands,
+        parent: Entity,
+        child_modifier: ChildModifier,
+    ) where
+        ChildModifier: FnMut(EntityCommands),
+    {
+        let offset = self.len();
+        self.text.push_str(s);
+        commands
+            .entity(parent)
+            .add_children(|builder| self.spawn_chars(builder, child_modifier, offset))
+    }
+
+    pub fn push_newline(&mut self) {
+        self.text.push('\n');
+    }
+
+    pub fn pop(&mut self, commands: &mut Commands) {
+        if self.text.pop() != Some('\n') {
+            self.chars
+                .pop()
+                .into_iter()
+                .for_each(|entity| commands.entity(entity).despawn());
+        }
     }
 }
