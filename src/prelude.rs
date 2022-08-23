f06688c60e50fec0f609e01aacc16c7faddfe6b6
Jonah <jnhev42@gmail.com>
2022-08-23 11:30:07 +0100

    physics orgainisation for interaction groups

--- src/prelude.rs
+++ src/prelude.rs
@@ -2,5 +2,9 @@ pub(crate) use crate::player::Player;
 pub(crate) use crate::state::GameState;
 pub(crate) use crate::utils;
 pub use bevy::prelude::*;
-pub use bevy_rapier3d::prelude as phys;
 pub use iyes_loopless::prelude::*;
+
+pub mod phys {
+    pub use crate::utils::phys::group;
+    pub use bevy_rapier3d::prelude::*;
+}
